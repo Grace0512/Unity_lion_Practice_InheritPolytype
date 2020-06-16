c96d9df1c149b6d1ad29f626e22a1f99a5fa139f
grace <lov011052@gmail.com>
2020-06-16 16:27:52 +0800

    作業練習

---
+++ InheritPolytype/Assets/Scripts/PeopleFarAttack.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PeopleFarAttack : PeopleTrack
+{
+    [Header("停止距離"),Range(1,10)]
+    public float stop = 3f;
+
+    [Header("子彈")]
+    public GameObject bullet;
+
+    [Header("冷卻"),Range(0.1f,3f)]
+    public float cd = 1.5f;
+
+    private float timer;
+
+    protected override void Start()
+    {
+        agent.stoppingDistance = stop;
+        target = GameObject.Find("殭屍").transform;
+    }
+
+    protected override void Track()
+    {
+        agent.SetDestination(target.position);
+        if (agent.remainingDistance <= stop) Attack(); //如果代理器.距離 < 停止距離就攻擊
+    }
+
+    private void Attack()
+    {
+        timer += Time.deltaTime;
+        if (timer>=cd)
+        {
+            timer = 0;
+            ani.SetTrigger("攻擊");
+            GameObject temp = Instantiate(bullet, transform.position + transform.forward + transform.up, transform.rotation);
+            Rigidbody rig = temp.AddComponent<Rigidbody>(); //添加元件
+            rig.AddForce(transform.forward * 1500);
+        }
+    }
+
+    
+}
---
+++ InheritPolytype/Assets/Scripts/PeopleRun.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// 會竄逃的人類
+/// </summary>
+public class PeopleRun : People
+{
+    /// <summary>
+    /// 最終座標
+    /// </summary>
+    private Vector3 final;
+    [Header("半徑"),Range(1,30)]
+    public float radis = 5;
+    
+    private void Update()
+    {
+        Flee();
+    }
+    /// <summary>
+    /// 竄逃
+    /// </summary>
+    private void Flee()
+    {
+        if(agent.remainingDistance<1.5f)
+        {
+        // 隨機座標 = 隨機.球內隨機點 *半徑 + 中心點
+        Vector3 pointPan = Random.insideUnitSphere * radis + transform.position;
+
+        //導覽網格碰撞 碰撞點
+        NavMeshHit hit;
+
+        // 導覽網格.樣本座標(座標,碰撞點,半徑,圖層)
+        // out 執行方法會將結果直接儲存到傳入的參數內
+        // 執行後會將取得的隨機點儲存在hit參數內
+        NavMesh.SamplePosition(pointPan, out hit, 5, 1);
+
+        //最終座標 = 碰撞點.座標
+        final = hit.position;
+        }
+        
+
+        agent.SetDestination(final);
+    }
+}
